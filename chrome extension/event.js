c874f271f17af539302e4b6ecea623bc20f34ef7
Giorgi Gzirishvili <giogziro95@gmail.com>
2017-03-30 23:28:22 +0400

    Initial commit

---
+++ chrome extension/event.js
@@ -0,0 +1,25 @@
+function doSearch(selectedText) {
+	var url = "https://dictionary.ge/ka/search/?q=" + selectedText;
+	chrome.tabs.query({
+		active: true,
+		lastFocusedWindow: true
+	}, function(tabs) {
+		var tab = tabs[0];
+		chrome.tabs.create({
+			url: url,
+			index: tab.index + 1
+		});
+	});
+}
+
+chrome.contextMenus.create({
+	id: "doSearch",
+	title: "Search Dictionary.ge for \"%s\"",
+	contexts: ["selection", "link"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(eventData){
+	if (eventData.menuItemId == "doSearch" && eventData.selectionText) {
+		doSearch(eventData.selectionText.toString());
+	}
+});
